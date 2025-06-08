653807519f3a2796c281f6ceec6c1de31345dbfa
ariefRamadhan13 <arieframadhon1313@gmail.com>
2025-06-08 21:13:17 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import Tes1  # Mengimpor aplikasi Downhole dari Downhole1.py
 import Tes2  # Mengimpor aplikasi ARIMA dari Tes2.py
+import Tes3
 
 
 def main():
@@ -9,7 +10,7 @@ def main():
     st.title("Prediksi Produksi Minyak dan Parameter Reservoir")
     
     # Pilihan aplikasi yang ingin dijalankan
-    app_choice = st.selectbox("Pilih Aplikasi", ["Prediksi Harga Minyak", "Analisis Downhole"])
+    app_choice = st.selectbox("Pilih Aplikasi", ["Prediksi Harga Minyak", "Analisis Downhole", "Lithology Prediction"])
     
     if app_choice == "Prediksi Harga Minyak":
         # Menjalankan aplikasi ARIMA dari Tes2.py
@@ -17,6 +18,9 @@ def main():
     elif app_choice == "Analisis Downhole":
         # Menjalankan aplikasi Downhole dari Downhole1.py
         Tes1.downhole_app()
+    elif app_choice == "Lithology Prediction":
+        # Menjalankan aplikasi Downhole dari Downhole1.py
+        Tes3.lithology_app()
 
 if __name__ == "__main__":
     main()
